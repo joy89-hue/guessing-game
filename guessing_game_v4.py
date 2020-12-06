fddf218134b81e9604ef7fe7dfafbc2c6b48bb6e
Joydeep Roy <63199831+joy89-hue@users.noreply.github.com>
2020-12-06 07:40:32 +0530

    Update guessing_game_v4.py

--- guessing_game_v4.py
+++ guessing_game_v4.py
@@ -1,7 +1,25 @@
 import random
 
-answer = random.randint(0,9)
-# print(secret_number)
-print("** Guess a number between 0 to 9 **")
-guess = int(input("Enter: "))
+highest = 1000
+secret_number = random.randint(1, highest)
+# print(secret_number) # TODO: Remove after testing
+guess = 0   # initialise to any number that doesn't equal to  answer
+print("Please guess a number between 1 and {}".format(highest))
 
+while guess != secret_number:
+    guess = int(input("Enter: "))
+    if guess == 0:
+        break
+    if guess == secret_number:
+        print("Well done, you guessed correctly!")
+        break
+    else:
+        if guess < secret_number:
+            print("Please guess higher")
+        else:
+            print("Please guess lower")
+        # guess = int(input("Enter: "))
+        # if guess == secret_number:
+        #     print("Well done, you guessed correctly!")
+        # else:
+        #     print("Sorry, you can't made it")
